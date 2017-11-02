e349b2f4324e2874a4490fb0dddd1aaa413b6194
Daniel Lacasse <daniel@gradle.com>
2017-11-01 21:33:26 -0400

    Try to fix Linux when using stderr

--- subprojects/testing-native/src/integTest/groovy/org/gradle/nativeplatform/test/xctest/XCTestTestFrameworkIntegrationTest.groovy
+++ subprojects/testing-native/src/integTest/groovy/org/gradle/nativeplatform/test/xctest/XCTestTestFrameworkIntegrationTest.groovy
@@ -114,7 +114,7 @@ allprojects { p ->
         }
 
         private static final String FAILING_TEST = """
-            fputs("some error output", __stderrp)
+            fputs("some error output", stderr)
             XCTAssert(false, "test failure message")
         """
 
